b348d017cf47ec0ba4bb32a55d8f985bf82c3dcd
Piyush <prince840888@gmail.com>
2025-05-28 15:02:41 +0530

    h

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -5,7 +5,7 @@ export const getAllUsers = async (req, res, next) => {
 	try {
 		const currentUserId = req.auth.userId;
 		const users = await User.find({ clerkId: { $ne: currentUserId } });
-		res.status(200).json([ users ]);
+		res.status(200).json(users );
         console.log(users)
 	} catch (error) {
 		next(error);
