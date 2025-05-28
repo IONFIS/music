aff0b38113dd34ac00bdd6a9077a8da0e94eaf50
Piyush <prince840888@gmail.com>
2025-05-28 14:57:04 +0530

    g

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -3,7 +3,7 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import { getAllUsers} from "../controllers/user.controller.js";
 
 const router = Router();
-router.get('/users',protectRoute,getAllUsers);
+router.get('/users',getAllUsers);
 //TO DO MESSEGES WITH THE USERS
 
   export default router;
